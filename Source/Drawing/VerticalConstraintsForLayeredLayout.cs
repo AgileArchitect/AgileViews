b85d1c78f1956d427ba0b9524858f00545039987
Tom Staijen <t.staijen@gmail.com>
2016-02-19 07:29:23 +0100

    include msagl source

---
+++ Source/Drawing/VerticalConstraintsForLayeredLayout.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Msagl.Core.DataStructures;
+
+namespace Microsoft.Msagl.Drawing {
+    internal class VerticalConstraintsForLayeredLayout {
+        internal readonly Set<Node> _maxLayerOfDrawingGraph = new Set<Node>();
+        internal readonly Set<Node> _minLayerOfDrawingGraph = new Set<Node>();
+
+        public void PinNodeToMaxLayer(Node node) {
+            _maxLayerOfDrawingGraph.Insert(node);
+        }
+
+        public void PinNodeToMinLayer(Node node)
+        {
+            System.Diagnostics.Debug.Assert(node != null);
+            _minLayerOfDrawingGraph.Insert(node);
+        }
+
+        /// <summary>
+        /// unpins a node from max layer
+        /// </summary>
+        /// <param name="node"></param>
+        internal void UnpinNodeFromMaxLayer(Node node)
+        {
+            _maxLayerOfDrawingGraph.Remove(node);
+        }
+
+        
+        /// <summary>
+        /// unpins a node from min layer
+        /// </summary>
+        /// <param name="node"></param>
+        internal void UnpinNodeFromMinLayer(Node node)
+        {
+            _minLayerOfDrawingGraph.Remove(node);
+        }
+
+
+        internal Set<Tuple<Node, Node>> SameLayerConstraints = new Set<Tuple<Node, Node>>();
+        internal readonly Set<Tuple<Node, Node>> UpDownConstraints = new Set<Tuple<Node, Node>>();
+        public void Clear() {
+            _maxLayerOfDrawingGraph.Clear();
+            _minLayerOfDrawingGraph.Clear();
+            SameLayerConstraints.Clear();
+            UpDownConstraints.Clear();
+        }
+    }
+}
---
+++ Source/MSAGL/Routing/Spline/Bundling/PointPairOrder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.Msagl.Routing.Spline.Bundling {
+    internal class PointPairOrder {
+        //order for node u of edge u->v
+        internal List<Metroline> Metrolines = new List<Metroline>();
+        internal bool orderFixed;
+        internal Dictionary<Metroline, int> LineIndexInOrder;
+
+        internal int Count { get { return Metrolines.Count; } }
+
+        internal void Add(Metroline metroline) {
+            Metrolines.Add(metroline);
+        }
+    }
+}
